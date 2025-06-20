29ecacd5a96cbcb90b26eba9ac0080e64855b760
Aliya K. <aliyakamaliya21@gmail.com>
2025-06-20 17:54:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ requireLogin();
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Gama StudyHub</title>
+        <title>StudyHub</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
         <link href="https://cdn.materialdesignicons.com/7.2.96/css/materialdesignicons.min.css" rel="stylesheet">
         <style>
